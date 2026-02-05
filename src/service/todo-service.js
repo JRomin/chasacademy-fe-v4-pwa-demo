bebf1f6f1ee25523516f04ede4fad72547a247d7
Johan Romin <johan.romin@chasacademy.se>
2026-02-05 13:58:30 +0100

    Gjort en robustare offline kö som kollar så bara objekt som inte gick att synca syncas igen.

--- src/service/todo-service.js
+++ src/service/todo-service.js
@@ -17,22 +17,36 @@ export async function saveTodo(todoContainer) {
     try {
         let todoTitle = todoContainer.querySelector('#title').value;
         let todoDescription = todoContainer.querySelector('#description').value;
-        console.log(todoTitle);
         await createTodo(self.crypto.randomUUID(), todoTitle, false);
         document.getElementById('addContainer').innerHTML = '';
     } catch (error) {
-        errorContainer.innerHTML = error.message;
+      console.error(error);
+      errorContainer.innerHTML = error.message;
     }
 }
 
 export async function syncOfflineTodos(loadingContainer, errorContainer) {
   let offlineCache = JSON.parse(localStorage.getItem("todos-queue")) || [];
   if (offlineCache.length == 0) return;
+  let syncFailed = false;
 
   for(let i=0;i<offlineCache.length;i++) {
     let currentTodoItem = offlineCache[i];
-    await createTodo(self.crypto.randomUUID(), currentTodoItem.title, currentTodoItem.done);
-  };
-  localStorage.removeItem("todos-queue");
-  await loadTodos(loadingContainer, errorContainer);
+    try {
+      await createTodo(currentTodoItem.todoItem.id, currentTodoItem.todoItem.title, currentTodoItem.todoItem.done);
+    } catch(error) {
+      syncFailed = true;
+      currentTodoItem.retries++;
+      offlineCache[i] = currentTodoItem;
+    }
+  }
+  console.log("Sync status: "+syncFailed);
+  if (!syncFailed) {
+    localStorage.removeItem("todos-queue");
+  }
+  try {
+    await loadTodos(loadingContainer, errorContainer);
+  } catch(error) {
+    console.log("Error when loading in sync: "+error);
+  }
 }
