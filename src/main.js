25e5a6b2ccb8b64291bef9850d366a79cbbb51a5
Johan Romin <johan.romin@chasacademy.se>
2026-02-05 13:57:28 +0100

    Lite utökningar av main för att visa felmeddelanden för användaren.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './style.css'
 // Service methods
 import { loadTodos, saveTodo, syncOfflineTodos } from './service/todo-service.js'
 import { renderAddTodoItem } from './views/todo-views.js';
+import { loginUser } from './api/auth-api.js';
 
 document.querySelector('#app').innerHTML = `
   <div class="container">
@@ -38,10 +39,39 @@ addButton.onclick = function() {
 loadTodos(loadingContainer, errorContainer);
 
 window.addEventListener("online", function() {
+  errorContainer.innerHTML = "";
   syncOfflineTodos(loadingContainer, errorContainer)
+});
+
+window.addEventListener("offline", function() {
+  errorContainer.innerHTML = "Offline!";
 });
 
 document.getElementById('colorMode').onchange = function(event) {
   let selectedColorMode = event.target.value;
   document.cookie="colorMode="+selectedColorMode+"; max-age=60; path=/";
 }
+
+/*
+* Nedan är testkod för ett inloggningsflöde!
+try {
+let isLoggedIn = await loginUser('johan','topsecret');
+if (isLoggedIn) {
+  console.log("User is logged in!");
+}
+}catch(error) {
+  if (error instanceof TypeError) {
+    errorContainer.innerHTML = 'Network error!';
+  }
+}*/
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      new Response(
+        JSON.stringify({ error: "offline" }),
+        { headers: { "Content-Type": "application/json" } }
+      )
+    )
+  );
+});
